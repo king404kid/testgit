51466b70a9778835fd07e26d8357322c6c8d93ec
king404kid <king404kid@qq.com>
2022-03-02 09:59:21 +0800

    提交测试配置文件

---
+++ Config/ActionConfig.lua
@@ -0,0 +1,14 @@
+-- AUTO GENERATED. DO NOT MODIFY !!!
+-- FROM FILE /action_config.xlsx
+-- FROM SHEET export_交互动作
+return {
+	{"动作Id","名称","品质","datalock解锁条件","使用条件","类型","重复获得永久时的转换奖励"},
+	{"ActionId","Name","QualityId","DataLock","StartCondition","Type","ExchangeBonus"},
+	{1001, "测试1", 1, 1, nil, 1, {{num=1,id=999,type=1}}},
+	{1002, "测试2", 1, 1, nil, 1, {{num=1,id=999,type=1}}},
+	{1003, "测试3", 1, 1, nil, 1, {{num=1,id=999,type=1}}},
+	{1004, "测试4", 1, 1, nil, 1, {{num=1,id=999,type=1}}},
+	{1005, "测试5", 1, 1, nil, 1, {{num=2,id=999,type=1}}},
+	{1006, "测试6", 1, 1, nil, 1, {{num=3,id=999,type=1}}},
+	{10001, "测试7", 1, 1, nil, 2, {{num=3,id=999,type=1}}},
+}
